023762b2024e052d58cf67fbaac52001be74c032
serafinasalim <105096651+serafinasalim@users.noreply.github.com>
2024-07-27 18:04:24 +0700

    Register User API

--- be/Controller/ControllerUser.go
+++ be/Controller/ControllerUser.go
@@ -5,7 +5,9 @@ import (
 	"net/http"
 	"strconv"
 
+	"hackjakarta/Dto"
 	repo "hackjakarta/Repository/users"
+	utils "hackjakarta/Utils"
 
 	"github.com/gin-gonic/gin"
 )
@@ -29,4 +31,55 @@ func DetailUser(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"status": "success", "user": detail})
+}
+
+func RegisterUser(c *gin.Context) {
+	var params Dto.RegisterUser
+	if err := c.ShouldBindJSON(&params); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": err.Error()})
+		return
+	}
+
+	// Validasi input menggunakan validator
+	if err := utils.Validate.Struct(params); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": err.Error()})
+		return
+	}
+
+	// Validasi unique email
+	exist, err := repo.CheckEmail(params.Email)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"status": "error", "message": err.Error()})
+		return
+	}
+
+	if exist {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Email already regostered"})
+		return
+	}
+
+	// Validasi unique username
+	exist, err = repo.CheckUsername(params.Username)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"status": "error", "message": err.Error()})
+		return
+	}
+
+	if exist {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Username unavailable"})
+		return
+	}
+
+	res, err := repo.RegisterUser(params)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"status": "error", "message": err.Error()})
+		return
+	}
+
+	if !res {
+		c.JSON(http.StatusInternalServerError, gin.H{"status": "error", "message": "Failed to register user"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "success", "message": "User registered successfully"})
 }
--- be/Repository/users/users.go
+++ be/Repository/users/users.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	Config "hackjakarta/Config"
+	"hackjakarta/Dto"
 )
 
 func DetailUser(id int64) (result User, err error) {
@@ -26,5 +27,58 @@ func DetailUser(id int64) (result User, err error) {
 		return
 	}
 
+	return
+}
+
+func RegisterUser(params Dto.RegisterUser) (result bool, err error) {
+	const query = `INSERT INTO public.users (
+						first_name,
+						last_name,
+						username,
+						email,
+						password
+					)
+					VALUES ($1, $2, $3, $4, $5)`
+
+	tx, err := Config.DB.Begin()
+	if err != nil {
+		return false, err
+	}
+	row := tx.QueryRow(
+		query,
+		params.FirstName,
+		params.LastName,
+		params.Username,
+		params.Email,
+		params.Password,
+	)
+
+	if err = row.Scan(
+		&result,
+	); err != nil {
+		tx.Rollback()
+		return
+	}
+
+	return
+}
+
+func CheckEmail(email string) (exist bool, err error) {
+	const query = `SELECT EXISTS(SELECT 1 FROM users WHERE email = $1)`
+	row := Config.DB.QueryRow(query, email)
+
+	if err = row.Scan(&exist); err != nil {
+		return
+	}
+	return
+}
+
+func CheckUsername(username string) (exist bool, err error) {
+	const query = `SELECT EXISTS(SELECT 1 FROM users WHERE username = $1)`
+	row := Config.DB.QueryRow(query, username)
+
+	if err = row.Scan(&exist); err != nil {
+		return
+	}
 	return
 }
