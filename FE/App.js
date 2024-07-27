491a0f3dbdda429c540b5af09af64a633d7ded49
Alex <alexcode0702@gmail.com>
2024-07-27 15:56:33 +0700

    navbar

--- FE/App.js
+++ FE/App.js
@@ -1,19 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import NavBar from './components/NavBar/NavBar';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class TestNavBar extends React.Component {
+  changePage = (screen) => {
+    console.log(screen + ' has been pressed');
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <NavBar changePage={this.changePage} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
