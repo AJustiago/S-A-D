c0f8c7731a21aac465464f6f9eb74f1bfca7acb0
Daniel Wiratman <74503671+danielwiratman@users.noreply.github.com>
2024-07-27 17:44:37 +0700

    finish adding icons and new screen

--- mobiletest/src/navigators/MainNavigator.tsx
+++ mobiletest/src/navigators/MainNavigator.tsx
@@ -1,21 +1,52 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import MapScreen from '../screens/MapScreen'
+import {View, Text} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MapScreen from '../screens/MapScreen';
+import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import ForumScreen from '../screens/ForumScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
 const MainNavigator = () => {
   return (
     <NavigationContainer>
-        <Tab.Navigator>
-            <Tab.Screen name="Forum" component={MapScreen} />
-            <Tab.Screen name="Map" component={MapScreen} />
-            <Tab.Screen name="Profile" component={MapScreen} />
-        </Tab.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Forum"
+          component={ForumScreen}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <FontAwesomeIcon name="comments" color={color} size={size} />
+            ),
+            headerShown: false,
+          }}
+        />
+        <Tab.Screen
+          name="Map"
+          component={MapScreen}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <FontAwesome5Icon name="map-marked-alt" color={color} size={size} />
+            ),
+            headerShown: false,
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <FontAwesome5Icon name="user-alt" color={color} size={size} />
+            ),
+            headerShown: false,
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
-export default MainNavigator
+export default MainNavigator;
